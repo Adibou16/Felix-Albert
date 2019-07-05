fd499db0b138ff5a81aecd929388066f25b87d06
Felix <filabert16@outlook.con>
2019-07-05 17:13:53 -0400

    Ajout de GG (ya un problem avec le css)

---
+++ games/GG/GG.js
@@ -0,0 +1,35 @@
+var guess = 0;
+var computer = (Math.random() * 100);
+var computer = Math.round(computer)
+var remaining = 10;
+
+var input1 = document.querySelector("#ipbox");
+var output1 = document.querySelector("#output");
+
+var btn = document.querySelector("button");
+
+btn.addEventListener("click", onMouseClick);
+
+function onMouseClick() {
+    remaining = remaining - 1;
+    guess = parseInt(input1.value);
+
+
+    if (guess > computer) {
+        output1.innerHTML = "My number is lower, you have " + remaining + " chances remaining.";
+        if (remaining < 1) {
+            gameEnd();
+        }
+    } else if (guess < computer) {
+        output1.innerHTML = "My number is higher , you have " + remaining + " chances remaining.";
+        if (remaining < 1) {
+            gameEnd();
+        }
+    } else if (guess == computer)
+        output1.innerHTML = "Good job, my number was " + computer;
+
+
+    function gameEnd() {
+        output1.innerHTML = "You lost, my number was " + computer;
+    }
+}
